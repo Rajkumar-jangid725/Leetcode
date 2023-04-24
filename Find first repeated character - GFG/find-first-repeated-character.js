fe625a4a810c9d302ec3220383f700adf8760d90
Rajkumar jangid <58415950+Rajkumar-jangid725@users.noreply.github.com>
2023-04-24 22:30:33 +0530

    Added solution - LeetHub

---
+++ Find first repeated character - GFG/find-first-repeated-character.js
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  let t = parseInt(readLine());
+  let i = 0;
+  for (; i < t; i++) {
+    let s = readLine();
+    let obj = new Solution();
+    let res = obj.firstRepChar(s);
+    console.log(res);
+  }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+class Solution {
+  firstRepChar(s) {
+    // code here
+    let set = new Set();
+    for(let i=0;i<s.length;i++){
+        let c = s[i];
+        if(set.has(c)){
+            return c;
+        }
+        else{
+            set.add(c);
+        }
+    }
+    return -1;
+  }
+}
